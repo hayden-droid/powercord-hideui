52a743ffab1b8afc1df3682c67196880d35a87aa
Frank Moskal <moskal.16@osu.edu>
2019-12-13 11:27:34 -0500

    v1.0.3
    Bug Fix: Fixed the addEventListener injection issue.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Plugin } = require("powercord/entities")
-const { getModule } = require("powercord/webpack")
-const {inject, uninject} = require("powercord/injector")
-const { resolve } = require("path");
+const {
+    Plugin
+} = require("powercord/entities")
+const {
+    resolve
+} = require("path");
 
 const Config = require("./config.jsx");
 const Injector = require("./injector.js")
@@ -21,59 +23,63 @@ module.exports = class HideUI extends Plugin {
             subtree: true
         }
 
-        const FriendsObserver = new MutationObserver(function(mutations) {
-        	if (Injector.ClickFriends()) {
-        		FriendsObserver.disconnect();
-        	}
+        const FriendsObserver = new MutationObserver(function(m) {
+            if (Injector.ClickFriends()) {
+                FriendsObserver.disconnect();
+            }
         });
-        if (prefs.get('chactivity')) { FriendsObserver.observe(document.body, Options); }
+        if (prefs.get('chactivity')) {
+            FriendsObserver.observe(document.body, Options);
+        }
 
-        const DiscoveryObserver = new MutationObserver(function(mutations) {
+        const DiscoveryObserver = new MutationObserver(function(m) {
             if (Injector.ToggleElement(prefs.get('gdiscovery'), Injector.StyleSheetsJS.get('gdiscovery'), 5, 'flex')) {
                 DiscoveryObserver.disconnect();
             }
         });
-        if (prefs.get('gdiscovery')) { DiscoveryObserver.observe(document.body, Options); }
+        if (prefs.get('gdiscovery')) {
+            DiscoveryObserver.observe(document.body, Options);
+        }
 
-        const CSSObserver = new MutationObserver(function(mutations) {
+        const CSSObserver = new MutationObserver(function(m) {
 
             let node = document.getElementById('powercord-css-powercord-hideui');
             if (node && node.textContent) {
                 CSSObserver.disconnect();
 
                 Injector.UpdateUI(Injector.StyleSheetsCSS.get('fhelp'), prefs.get('fhelp'), 'flex');
-                
+
                 const Keys = new Set(Injector.StyleSheetsCSS.keys());
                 Keys.delete('fhelp');
                 Keys.delete('sbseparator');
-                
+
                 for (const key of Keys) {
-                	Injector.UpdateUI(Injector.StyleSheetsCSS.get(key), prefs.get(key));
+                    Injector.UpdateUI(Injector.StyleSheetsCSS.get(key), prefs.get(key));
                 }
 
-                Injector.UpdateUI(Injector.StyleSheetsCSS.get('sbseparator'), prefs.get('sbnitro') &&  prefs.get('sbserver') && prefs.get('sbhype'));
+                Injector.UpdateUI(Injector.StyleSheetsCSS.get('sbseparator'), prefs.get('sbnitro') && prefs.get('sbserver') && prefs.get('sbhype'));
                 Injector.UpdateSocial(prefs.get('sbsocial'));
             }
         });
         CSSObserver.observe(document.body, Options);
 
-        const SettingsObserver = new MutationObserver(function(mutations){
-            let node = document.querySelector('div[class^="sidebar"] > div[class^="panels"] button[aria-label="User Settings"]');
-            if (node) {
+        const SettingsObserver = new MutationObserver(function(m) {
+            if (document.querySelector('div[class^="sidebar"] > div[class^="panels"] button[aria-label="User Settings"]')) {
                 SettingsObserver.disconnect();
-                node.addEventListener("click", function(){
-                    const Observer = new MutationObserver(function(mutations) {
-                        let node = document.querySelector('nav[class^="sidebar"] div[class^="header"]');
-                        if (node) {
-                            Observer.disconnect();
-                            for (const key of Injector.StyleSheetsSB.keys()) {
-                                Injector.UpdateSB(Injector.StyleSheetsSB.get(key), prefs.get(key));
+                setTimeout(() => {
+                    document.querySelector('div[class^="sidebar"] > div[class^="panels"] button[aria-label="User Settings"]').addEventListener('click', function(e) {
+                        const Observer = new MutationObserver(function(m) {
+                            if (document.querySelector('nav[class^="sidebar"] div[class^="header"]')) {
+                                Observer.disconnect();
+                                for (const key of Injector.StyleSheetsSB.keys()) {
+                                    Injector.UpdateSB(Injector.StyleSheetsSB.get(key), prefs.get(key));
+                                }
+                                Injector.UpdateUI(Injector.StyleSheetsCSS.get('sbseparator'), prefs.get('sbnitro') && prefs.get('sbserver') && prefs.get('sbhype'));
                             }
-                            Injector.UpdateUI(Injector.StyleSheetsCSS.get('sbseparator'), prefs.get('sbnitro') &&  prefs.get('sbserver') && prefs.get('sbhype'));                            
-                        }
+                        });
+                        Observer.observe(document.body, Options);
                     });
-                    Observer.observe(document.body, Options);
-                });
+                }, 1000);
             }
         });
         SettingsObserver.observe(document.body, Options);
